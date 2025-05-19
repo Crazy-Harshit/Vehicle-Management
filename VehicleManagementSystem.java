2264054d2177a7e25702c824912caf9314177fb2
Harshit Sharma <harshitsharma958073@gmail.com>
2025-05-19 21:05:42 +0530

    Update VehicleManagementSystem.java

--- VehicleManagementSystem.java
+++ VehicleManagementSystem.java
@@ -3,15 +3,15 @@ public class VehicleManagementSystem {
         Vehicle car = new Car("Toyota", 4);
         Vehicle bike = new Bike("Yamaha", true);
         Vehicle truck = new Truck("Volvo", 18.5);
-        System.out.println("=== Car Details ===");
+        System.out.println(" Car Details ");
         car.start();
         car.displayInfo();
         car.stop();
-        System.out.println("\n=== Bike Details ===");
+        System.out.println("\n Bike Details ");
         bike.start();
         bike.displayInfo();
         bike.stop();
-        System.out.println("\n=== Truck Details ===");
+        System.out.println("\n Truck Details ");
         truck.start();
         truck.displayInfo();
         truck.stop();
